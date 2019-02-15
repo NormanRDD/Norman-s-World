b8aec631dd314811b81e5e0f215796f2ff1f8fee
NormanRDD <43154261+NormanRDD@users.noreply.github.com>
2019-02-16 01:15:34 +0600

    Add files via upload

---
+++ Lab2/Lab2Task4/Lab2Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\Desktop\milkyway3\yyyy\1111\rrrr.txt";
+            string path2 = @"C:\Users\User\Desktop\milkyway3\wwww\qqqq\tttt.txt";
+
+            if (File.Exists(path2))
+            //Проверяем есть ли существующий файл 
+            //Exists(file): определяет, существует ли файл
+            {
+                File.Delete(path2);     //Если есть ,то удаляем.
+                File.Move(path, path2);    //После файл с path перемещяем в path2
+            }
+        }
+    }
+}
